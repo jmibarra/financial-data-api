4595bc0ca02830445c21462c8c25b64d96d0d762
Juan Ibarra <jmibarra86@gmail.com>
2024-05-25 20:02:54 -0300

    Fix para la api de dividendos

--- getDividendInfo.js
+++ getDividendInfo.js
@@ -10,7 +10,7 @@ const fetchStockData = async (symbol) => {
         const calendarEventsResult = await yahooFinance.quoteSummary(symbol, { modules: ["calendarEvents"] });
 
         // Extraer las fechas
-        let dividendDateFormatted = result.dividendDate ? format(new Date(result.dividendDate), 'dd/MM/yyyy') : "";
+        let dividendDateFormatted = calendarEventsResult.calendarEvents.dividendDate ? format(new Date(calendarEventsResult.calendarEvents.dividendDate), 'dd/MM/yyyy') : "";
         let exDividendDateFormatted = calendarEventsResult.calendarEvents.exDividendDate ? format(new Date(calendarEventsResult.calendarEvents.exDividendDate), 'dd/MM/yyyy') : "";
 
         // Extraer otros datos
@@ -18,6 +18,7 @@ const fetchStockData = async (symbol) => {
 
         // Devolver todos los datos requeridos
         return { dividendDate: dividendDateFormatted, dividendYield, dividendRate, exDividendDate: exDividendDateFormatted };
+
     } catch (error) {
         console.error('Error fetching stock data:', error);
         return null;
